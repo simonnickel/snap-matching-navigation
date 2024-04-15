f0a5baadaf622bd48702949f834abe911ce07c1f
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 12:13:58 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "snap-matching-navigation",
+	platforms: [
+		.iOS(.v17), .macOS(.v14)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SnapMatchingNavigation"),
+            name: "SnapMatchingNavigation",
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]
+		),
         .testTarget(
             name: "SnapMatchingNavigationTests",
             dependencies: ["SnapMatchingNavigation"]),
