186b6922fb11b86f3301c4d79774c9251b3d0330
Simon Nickel <simonnickel@googlemail.com>
2024-04-18 20:08:42 +0200

    Add Demo project, created with Xcode 15.3

---
+++ SnapMatchingNavigationDemo/SnapMatchingNavigationDemo/SnapMatchingNavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapMatchingNavigationDemoApp.swift
+//  SnapMatchingNavigationDemo
+//
+//  Created by Simon Nickel on 18.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SnapMatchingNavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
